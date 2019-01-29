999b52c85a4e5301ceae2f59dea0200b0d2830e3
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-01-28 16:55:47 -0800

    Changed Base Url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'MC Design Specs',
   description: 'A Collaborative Endeavour',
-  base: '/spec/',
+  base: '/busi/',
   head: [
     ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `favicon.ico` }]
   ],
